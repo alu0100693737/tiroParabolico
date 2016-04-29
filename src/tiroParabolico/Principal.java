b66ac6ade6d0c0f4e96cd2ed57a7d70ff301d4b2
alu0100693737 <alu0100693737@ull.edu.es>
2016-04-29 23:40:57 +0200

    Trabajando con paneles

--- src/tiroParabolico/Principal.java
+++ src/tiroParabolico/Principal.java
@@ -11,6 +11,7 @@ public class Principal {
 		prueba.setLocationRelativeTo(null); // Center the frame
 		prueba.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		prueba.setVisible(true);
+		System.out.println("Ventana" + prueba.getWidth() + " " + prueba.getHeight());
 	}
 
 }
--- src/tiroParabolico/pnlAplicacion.java
+++ src/tiroParabolico/pnlAplicacion.java
@@ -1,14 +1,42 @@
 package tiroParabolico;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
 
 import javax.swing.JPanel;
 
 public class pnlAplicacion extends JPanel {
 	
+	private final static int MARGEN = 50;
+	private final static int PALITO = 5;
+	
 	public pnlAplicacion() {
+		System.out.println("pepe");
+		
 		setPreferredSize(new Dimension(800, 500));
-
 	}
-
+	public void paint(Graphics g){ 
+		    Graphics2D g2 = (Graphics2D) g.create();
+		     System.out.println(getWidth()  + " " + getHeight());
+		    g2.drawLine(MARGEN, getHeight() - MARGEN, MARGEN, MARGEN); 
+		    g2.drawLine(MARGEN, getHeight() - MARGEN, getWidth() - MARGEN, getHeight() - MARGEN ); 
+		    for (int i = MARGEN; i < (getWidth() - MARGEN ); i+=10) {
+		    	g2.drawLine(i, getHeight() - MARGEN, i, getHeight() - MARGEN + PALITO);
+					
+					if(i % 50 == 0) {
+						g2.drawString( Integer.toString((i/10)) + "0", MARGEN - 10 + i, getHeight() - MARGEN + 3 * PALITO);
+						}
+				}
+		    
+		    for (int i = (getHeight() - MARGEN); i > MARGEN; i-=10) {
+		    	g2.drawLine(MARGEN, i, MARGEN - PALITO, i);
+					if((getHeight()- i) % 50 == 0) {
+						g2.drawString(Integer.toString((i/10)) + "0", MARGEN - 30, getHeight() - i - MARGEN + PALITO);
+						}
+				}
+	}
 }
